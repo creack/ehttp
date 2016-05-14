9b61518cb7ef04d4540ec49df9914a83c3298b2e
Guillaume J. Charmes <gcharmes@leaf.ag>
2016-05-14 08:00:51 -0500

    Fix ehttprouter
    Signed-off-by: Guillaume J. Charmes <gcharmes@leaf.ag>

--- ehttprouter/ehttprouter_test.go
+++ ehttprouter/ehttprouter_test.go
@@ -1,10 +1,13 @@
 package ehttprouter
 
 import (
+	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"net/http"
 	"net/http/httptest"
+	"path"
+	"runtime"
 	"strings"
 	"testing"
 
@@ -13,16 +16,34 @@ import (
 )
 
 func assertInt(t *testing.T, expect, got int) {
+	_, file, line := getCallstack(1)
 	if expect != got {
-		t.Errorf("Unexpected result.\nExpect:\t%d\nGot:\t%d\n", expect, got)
+		t.Errorf("[%s:%d] Unexpected result.\nExpect:\t%d\nGot:\t%d\n", file, line, expect, got)
 	}
 }
 
 func assertString(t *testing.T, expect, got string) {
+	_, file, line := getCallstack(1)
 	expect, got = strings.TrimSpace(expect), strings.TrimSpace(got)
 	if expect != got {
-		t.Errorf("Unexpected result.\nExpect:\t%s\nGot:\t%s\n", expect, got)
+		t.Errorf("[%s:%d] Unexpected result.\nExpect:\t%s\nGot:\t%s\n", file, line, expect, got)
 	}
+}
+
+func assertJSONError(t *testing.T, expect, got string) {
+	_, file, line := getCallstack(1)
+	expect, got = strings.TrimSpace(expect), strings.TrimSpace(got)
+
+	jErr := ehttp.JSONError{}
+	if err := json.Unmarshal([]byte(got), &jErr); err != nil {
+		t.Errorf("[%s:%d] Error parsing json error: %s\n", file, line, expect)
+	}
+	for _, errStr := range jErr.Errors {
+		if errStr == expect {
+			return
+		}
+	}
+	t.Errorf("[%s:%d] Unexpected error.\nExpect:\t%s\nGot:\t%s\n", file, line, expect, got)
 }
 
 func TestMWErrorPanicCommon(t *testing.T) {
@@ -45,7 +66,7 @@ func TestMWErrorPanicCommon(t *testing.T) {
 	if err != nil {
 		t.Fatal(err)
 	}
-	assertString(t, "fail", string(body))
+	assertJSONError(t, "fail", string(body))
 }
 
 func TestMWErrorPanicEHTTP(t *testing.T) {
@@ -68,7 +89,7 @@ func TestMWErrorPanicEHTTP(t *testing.T) {
 	if err != nil {
 		t.Fatal(err)
 	}
-	assertString(t, "fail", string(body))
+	assertJSONError(t, "fail", string(body))
 }
 
 func TestMWErrorPanicInt(t *testing.T) {
@@ -91,5 +112,18 @@ func TestMWErrorPanicInt(t *testing.T) {
 	if err != nil {
 		t.Fatal(err)
 	}
-	assertString(t, "(int) 418", string(body))
+	assertJSONError(t, "(int) 418", string(body))
+}
+
+func getCallstack(skip int) (string, string, int) {
+	var name string
+	pc, file, line, ok := runtime.Caller(1 + skip)
+	if !ok {
+		name, file, line = "<unkown>", "<unknown>", -1
+	} else {
+		name = runtime.FuncForPC(pc).Name()
+		name = path.Base(name)
+		file = path.Base(file)
+	}
+	return name, file, line
 }
